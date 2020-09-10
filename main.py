b64b04c7a6aa27d435b09dfe73798ef6abd0d660
Sergio Ulloa <siu5033@psu.edu>
2020-09-10 23:35:40 +0000

    last touches

--- main.py
+++ main.py
@@ -6,27 +6,28 @@
 # Breakout: 10
 
 def getLetterGrade(grade):
-  if(grade >= 93.0)
-  lettergrade = "A"
-  elif (grade >= 90.0)
-  lettergrade = "A-"
-  elif (grade >= 87.0)
-  lettergrade = "B+"
-  elif (grade	>=83.0)
-  lettergrade = "B"
-  elif (grade >= 80.0)
-  lettergrade = "B-"
-  elif (grade >= 77.0)
-  lettergrade = "C+"
-  elif (grade >= 70.0)
-  lettergrade = "C"
-  elif (grade >= 60.0)
-  lettergrade = "D"
-  else
-  lettergrade = "F"
+  if(grade >= 93.0):
+    lettergrade = "A"
+  elif (grade >= 90.0):
+    lettergrade = "A-"
+  elif (grade >= 87.0):
+    lettergrade = "B+"
+  elif (grade	>=83.0):
+    lettergrade = "B"
+  elif (grade >= 80.0):
+    lettergrade = "B-"
+  elif (grade >= 77.0):
+    lettergrade = "C+"
+  elif (grade >= 70.0):
+   lettergrade = "C"
+  elif (grade >= 60.0):
+    lettergrade = "D"
+  else:
+    lettergrade = "F"
+  return lettergrade
 def run():
-  num = 
-  num =
+  grade = input("Enter your CMPSC 131 grade: ")
+  grade = float(grade)
   print (f"Your letter grade for CMPS 131 is {getLetterGrade}.")
 
 if __name__ == "__main__":
