2dbd2b15f859078641fc725105dda2ad815fa46e
Sergio Ulloa <siu5033@psu.edu>
2020-09-10 20:46:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Author: Sergio Ulloa siu5033@psu.edu
+# Collaborator: siu5033@psu.edu
+# Collaborator: siu5033@psu.edu
+# Collaborator: siu5033@psu.edu
+# Section: 1
+# Breakout: 12
+
+int grade
+
+def getLetterGrade(grade):
+  if(grade >= 93.0)
+  lettergrade = "A"
+  elif (grade >= 90.0)
+  lettergrade = "A-"
+  elif (grade >= 87.0)
+  lettergrade = "B+"
+  elif (grade	>=83.0)
+  lettergrade = "B"
+  elif (grade >= 80.0)
+  lettergrade = "B-"
+  elif (grade >= 77.0)
+  lettergrade = "C+"
+  elif (grade >= 70.0)
+  lettergrade = "C"
+  elif (grade >= 60.0)
+  lettergrade = "D"
+  else
+  lettergrade = "F"
